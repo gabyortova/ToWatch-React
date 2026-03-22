5c400a42923cdbc77688ddab51fcc3fd5dd661e1
gabyortova <gabi.ortova@gmail.com>
2026-03-22 18:43:39 +0200

    update db.js

--- server2/config/db.js
+++ server2/config/db.js
@@ -1,11 +1,6 @@
-const config = require('./config');
-const mongoose = require('mongoose');
+const config = require("./config");
+const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect(config.dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false
-  });
+  return mongoose.connect(config.dbURL);
 };
